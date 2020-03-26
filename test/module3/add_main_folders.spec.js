9eda2da2a071e0419cc9672dc4cc77639ed5333d
Emmanuel Henri <manny@algofields.com>
2020-03-26 14:12:46 -0400

    test folders & scripts

---
+++ test/module3/add_main_folders.spec.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const assert = require("assert");
+
+describe('Folder structure is completed', () => {
+  it('The proper files exist in their respective folders @folders', () => {
+    assert(fs.existsSync(path.join(process.cwd(), 'src/controllers/controllers.js')), 'The controller file does not exist.');
+    assert(fs.existsSync(path.join(process.cwd(), 'src/models/models.js')), 'The Model file does not exist.');
+    assert(fs.existsSync(path.join(process.cwd(), 'src/routes/routes.js')), 'The Routes file does not exist.');
+  });
+});
---
+++ test/module4/pull_list.spec.js
@@ -0,0 +1,37 @@
+const assert = require("assert");
+const Product = require("../test_util.js");
+
+describe('Pulling products', () => {
+    let watch2;
+    
+    before((done) => {
+        watch2 = new Product({ name: 'Old Watch', description: 'a new watch' });
+        watch2.save()
+            .then(() => done())
+            .catch((err) => {
+                console.error("Handling promise rejection", err);
+            });
+    });
+
+    it('Add controler to pull a list of items @controler-lists', (done) => {
+        Product.findOne({ name: 'Old Watch' })
+            .then(() => {
+                assert(watch2.name === 'Old Watch'); 
+                done();
+            })
+            .catch((err) => {
+                console.error("Handling promise rejection", err);
+            });
+    })
+
+    it('Add route to pull a list of items @route-lists', (done) => {
+        Product.findOne({ name: 'Old Watch' })
+            .then(() => {
+                assert(watch2.name === 'Old Watch'); 
+                done();
+            })
+            .catch((err) => {
+                console.error("Handling promise rejection", err);
+            });
+    });
+});
