8c66ab3845b100cdddddb4aab2e4e2698a376870
Emmanuel Henri <manny@algofields.com>
2020-03-27 10:32:06 -0400

    testing updated

--- test/module2/test_server.spec.js
+++ test/module2/test_server.spec.js
@@ -1,22 +1,26 @@
-const request = require("request");
+const axios = require("axios");
 const expect = require("expect.js");
 
 const base_url = "http://localhost:4000/"
 
 describe("Server Running", function() {
   describe("GET /", function() {
-    it("returns status code 200 @connection", function(done) {
-      request.get(base_url, function(error, response, body) {
-        expect(response.statusCode).to.be(200);
-        done();
-      });
+    it("returns status code 200 @connection", (done) => {
+      axios.get(base_url)
+        .then((response) => {
+            expect(response.status).to.be(200)
+            done();
+        })
+        .catch((error) => done(error));
     });
 
-    it("returns Store server running message", function(done) {
-      request.get(base_url, function(error, response, body) {
-        expect(body).to.be("Store server running on port 4000");
-        done();
-      });
+    it("returns Store server running message", (done) => {
+      axios.get(base_url)
+      .then((response) => {
+          expect(response.data).to.be("Store server running on port 4000")
+          done();
+      })
+      .catch((error) => done(error));
     });
   });
 });
--- test/module3/add_single.spec.js
+++ test/module3/add_single.spec.js
@@ -1,52 +1,43 @@
-const assert = require("assert");
-const Product = require("../test_util.js");
+const axios = require("axios");
+const expect = require("expect.js");
 
 describe('Creating documents', () => {
-    let watch2;
-    
-    before((done) => {
-        watch2 = new Product({ name: 'Old Watch', description: 'a new watch' });
-        watch2.save()
-            .then(() => done())
-            .catch((err) => {
-                console.error("Handling promise rejection", err);
-            });
-    });
+    const url = 'http://localhost:4000/products';
 
-    it('Create the model and schema @create-model', (done) => {
-        const watch = new Product({ name: 'New watch', description: 'a new watch' });
-        watch.save()
-            .then(() => {
-                assert(!watch.isNew);
-                done();
-            })
-            .catch((err) => {
-                console.error("Handling promise rejection", err);
-            });
+    it('Create the model and schema @model-create', (done) => {
+        let watch = { name: 'Old Watch', description: 'a new watch' };
+        
+        axios.post(url, watch)
+        .then((response) => {
+            expect(response.data.name).to.contain("Old Watch");
+            done();
+        })
+        .catch((error) => done(error));
+        done();
     })
 
-    it('creates controler for route @create-controler', (done) => {
-        const watch = new Product({ name: 'New watch', description: 'a new watch' });
-        watch.save()
-            .then(() => {
-                assert(!watch.isNew);
-                done();
-            })
-            .catch((err) => {
-                console.error("Handling promise rejection", err);
-            });
+    it('creates controler for route @controller-create', (done) => {
+        let watch = { name: 'Old Watch', description: 'a new watch' };
+        
+        axios.post(url, watch)
+        .then((response) => {
+            expect(response.data.name).to.contain("Old Watch");
+            done();
+        })
+        .catch((error) => done(error));
+        done();
     })
 
-    it('creates a new product @create-product', (done) => {
-        const watch = new Product({ name: 'New watch', description: 'a new watch' });
-        watch.save()
-            .then(() => {
-                assert(!watch.isNew);
-                done();
-            })
-            .catch((err) => {
-                console.error("Handling promise rejection", err);
-            });
+    it('creates a new product @route-create', (done) => {
+        let watch = { name: 'Old Watch', description: 'a new watch' };
+        
+        axios.post(url, watch)
+        .then((response) => {
+            expect(response.data.name).to.contain("Old Watch");
+            done();
+        })
+        .catch((error) => done(error));
+        done();
     });
 });
 
