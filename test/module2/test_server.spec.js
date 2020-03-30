857a53f0218f3eb824d404f1cd7d83d759a3acc6
Manny Henri <mannyhenri@users.noreply.github.com>
2020-03-30 14:37:37 -0400

    updating error messages

--- test/module2/test_server.spec.js
+++ test/module2/test_server.spec.js
@@ -3,14 +3,23 @@ const expect = require('expect.js');
 
 describe("Server tests", function () {
   it("returns status code 200 @connection", function (done) {
-    expect(fs.readFileSync('index.js').toString())
-      .to.contain("res.send(`Store server running on port ${PORT}`)")
-    done();
+    try {
+      expect(fs.readFileSync('index.js').toString())
+        .to.contain("res.send(`Store server running on port ${PORT}`)")
+      done();
+    } catch (error) {
+      done(new Error("No response to GET call in your code"));
+    }
+
   })
 
   it("returns Store server running message", function (done) {
-    expect(fs.readFileSync('index.js').toString())
-      .to.contain("console.log(`Your server is running on port ${PORT}`)")
-    done();
+    try {
+      expect(fs.readFileSync('index.js').toString())
+        .to.contain("console.log(`Your server is running on port ${PORT}`)")
+      done();
+    } catch (error) {
+      done(new Error("No server logs when starting the server"));
+    }
   });
 });
--- test/module5/delete_item.spec.js
+++ test/module5/delete_item.spec.js
@@ -3,16 +3,26 @@ const expect = require('expect.js');
 
 describe('Deleting a product', () => {
     it('Add a controler to remove a product @controller-remove', (done) => {
-        expect(fs.readFileSync('src/controllers/controllers.js').toString())
-            .to.contain("Product.deleteOne({ _id: req.params.ProductID}, (err, Product) => {")
-        expect(fs.readFileSync('src/controllers/controllers.js').toString())
-            .to.contain("res.json({ message: 'successfully deleted product'});")
-        done();
+        try {
+            expect(fs.readFileSync('src/controllers/controllers.js').toString())
+                .to.contain("Product.deleteOne({ _id: req.params.ProductID}, (err, Product) => {")
+            expect(fs.readFileSync('src/controllers/controllers.js').toString())
+                .to.contain("res.json({ message: 'successfully deleted product'});")
+            done();
+        } catch (error) {
+            done(new Error("No controllers for DELETE endpoint detected"));
+        }
     })
 
     it('Add a route to remove a product @route-remove', (done) => {
-        expect(fs.readFileSync('src/routes/routes.js').toString())
-            .to.contain(".delete(deleteProduct);")
-        done();
+        try {
+            expect(fs.readFileSync('src/routes/routes.js').toString())
+                .to.contain(".delete(deleteProduct);")
+            done();
+
+        } catch (error) {
+            done(new Error("No routes for DELETE endpoint detected"));
+        }
+
     })
 });
