9ba78b7f2c2bd67dbe9a2f5a26c2c2762b31660b
Manny Henri <mannyhenri@users.noreply.github.com>
2020-03-30 14:38:53 -0400

    updating error messages

--- test/module4/pull_list.spec.js
+++ test/module4/pull_list.spec.js
@@ -3,16 +3,26 @@ const expect = require('expect.js');
 
 describe('Pulling products', () => {
     it('Add controler to pull a list of items @controller-lists', (done) => {
-        expect(fs.readFileSync('src/controllers/controllers.js').toString())
+        try {
+            expect(fs.readFileSync('src/controllers/controllers.js').toString())
             .to.contain("export const getProducts = (req, res) => {")
-        expect(fs.readFileSync('src/controllers/controllers.js').toString())
-            .to.contain("Product.find({}, (err, Product) => {")
-        done();
+            expect(fs.readFileSync('src/controllers/controllers.js').toString())
+                .to.contain("Product.find({}, (err, Product) => {")
+            done();   
+        } catch (error) {
+            done(new Error("No controllers for GET endpoint detected"));
+        }
+
     })
 
     it('Add route to pull a list of items @route-lists', (done) => {
-        expect(fs.readFileSync('src/routes/routes.js').toString())
+        try {
+            expect(fs.readFileSync('src/routes/routes.js').toString())
             .to.contain(".get(getProducts)")
-        done();
+            done();
+        } catch (error) {
+            done(new Error("No routes for GET endpoint detected"));
+        }
+
     })
 });
--- test/module4/pull_single.spec.js
+++ test/module4/pull_single.spec.js
@@ -3,18 +3,26 @@ const expect = require('expect.js');
 
 describe('Find a single product', () => {
     it('Add controler to pull a single item @controller-single', (done) => {
-        expect(fs.readFileSync('src/controllers/controllers.js').toString())
-            .to.contain("export const getProductWithID = (req, res) => {")
-        expect(fs.readFileSync('src/controllers/controllers.js').toString())
-            .to.contain("Product.findById(req.params.ProductID, (err, Product) => {")
-        done();
+        try {
+            expect(fs.readFileSync('src/controllers/controllers.js').toString())
+                .to.contain("export const getProductWithID = (req, res) => {")
+            expect(fs.readFileSync('src/controllers/controllers.js').toString())
+                .to.contain("Product.findById(req.params.ProductID, (err, Product) => {")
+            done();
+        } catch (error) {
+            done(new Error("No controllers for GET specific ID endpoint detected"));
+        }
     })
 
     it('Add route to pull a single item @route-single', (done) => {
-        expect(fs.readFileSync('src/routes/routes.js').toString())
-            .to.contain("app.route('/products/:ProductID')")
-        expect(fs.readFileSync('src/routes/routes.js').toString())
-            .to.contain(".get(getProductWithID)")
-        done();
+        try {
+            expect(fs.readFileSync('src/routes/routes.js').toString())
+                .to.contain("app.route('/products/:ProductID')")
+            expect(fs.readFileSync('src/routes/routes.js').toString())
+                .to.contain(".get(getProductWithID)")
+            done();
+        } catch (error) {
+            done(new Error("No routes for GET endpoint detected"));
+        }
     })
 });
