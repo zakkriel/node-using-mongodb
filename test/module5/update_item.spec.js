780060fa77bdd451be6c132a8efccf1edf19bc27
Manny Henri <mannyhenri@users.noreply.github.com>
2020-03-30 10:16:21 -0400

    tests updated

--- test/module5/update_item.spec.js
+++ test/module5/update_item.spec.js
@@ -1,38 +1,16 @@
-const expect = require("expect.js");
-const axios = require("axios");
+const fs = require('fs');
+const expect = require('expect.js');
 
 describe('Updating a product', () => {
-    const url = 'http://localhost:4000/products';
-
     it('Add a controler to update a product @controller-update', (done) => {
-        let cup = { name: 'Big Cup', description: 'a new cup' };
-        let updatedCup = { name: 'Big Blue Cup', description: 'a new cup' };
-
-        axios.post(url, cup)
-        .then((response) => {
-            axios.put(`${url}/${response.data._id}`, updatedCup)
-            .then((response) => {
-                expect(response.data.name).to.contain("Big Blue Cup")
-                done()
-            })  
-        })
-        .catch((error) => done(error));
+        expect(fs.readFileSync('src/controllers/controllers.js').toString())
+            .to.contain("Product.findOneAndUpdate({ _id: req.params.ProductID}, req.body, { new: true, useFindAndModify: false }, (err, Product)")
         done();
     })
 
     it('Add a controler to update a product @route-update', (done) => {
-        let cup = { name: 'Big Cup', description: 'a new cup' };
-        let updatedCup = { name: 'Big Blue Cup', description: 'a new cup' };
-
-        axios.post(url, cup)
-        .then((response) => {
-            axios.put(`${url}/${response.data._id}`, updatedCup)
-            .then((response) => {
-                expect(response.data.name).to.contain("Big Blue Cup")
-                done()
-            })  
-        })
-        .catch((error) => done(error));
+        expect(fs.readFileSync('src/routes/routes.js').toString())
+            .to.contain(".put(updateProduct)")
         done();
     })
-})
+});
