2a4ec681aaf6df4d4355c5c9c9dd061712e41195
Manny Henri <mannyhenri@users.noreply.github.com>
2020-03-30 10:15:40 -0400

    tests updated

--- test/module4/pull_single.spec.js
+++ test/module4/pull_single.spec.js
@@ -1,37 +1,20 @@
-const expect = require("expect.js");
-const axios = require("axios");
+const fs = require('fs');
+const expect = require('expect.js');
 
 describe('Find a single product', () => {
-    const url = 'http://localhost:4000/products';
-
     it('Add controler to pull a single item @controller-single', (done) => {
-        let watch = { name: 'Blue Watch', description: 'a new watch' };
-
-        axios.post(url, watch)
-        .then((response) => {
-            axios.get(`${url}/${response.data._id}`)
-            .then((response) => {
-                expect(response.data.name).to.contain("Blue Watch");
-                done();
-            })
-        })
-        .catch((error) => done(error));
+        expect(fs.readFileSync('src/controllers/controllers.js').toString())
+            .to.contain("export const getProductWithID = (req, res) => {")
+        expect(fs.readFileSync('src/controllers/controllers.js').toString())
+            .to.contain("Product.findById(req.params.ProductID, (err, Product) => {")
         done();
     })
 
     it('Add route to pull a single item @route-single', (done) => {
-        let watch = { name: 'Red Watch', description: 'a new watch' };
-
-        axios.post(url, watch)
-        .then((response) => {
-            axios.get(`${url}/${response.data._id}`)
-            .then((response) => {
-                expect(response.data.name).to.contain("Red Watch");
-                done();
-            })
-        })
-        .catch((error) => done(error));
+        expect(fs.readFileSync('src/routes/routes.js').toString())
+            .to.contain("app.route('/products/:ProductID')")
+        expect(fs.readFileSync('src/routes/routes.js').toString())
+            .to.contain(".get(getProductWithID)")
         done();
     })
-
 });
