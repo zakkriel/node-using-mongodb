0485c713191a14e591002e39259b57c1721ae730
Emmanuel Henri <manny@algofields.com>
2020-05-01 14:05:24 -0400

    update tests module 5

--- test/module5/update_item.spec.js
+++ test/module5/update_item.spec.js
@@ -5,7 +5,7 @@ describe('Updating a product', () => {
     it('Add a controler to update a product @controller-update', (done) => {
         try {
             expect(fs.readFileSync('src/controllers/controllers.js').toString())
-            .to.contain("Product.findOneAndUpdate({ _id: req.params.ProductID}, req.body, { new: true, useFindAndModify: false }, (err, Product)")
+            .to.contain("Product.findOneAndUpdate({ _id: req.params.ProductID }, req.body, { new: true, useFindAndModify: false }, (err, Product)")
             done(); 
         } catch (error) {
             done(new Error("No controllers for PUT endpoint detected"));
