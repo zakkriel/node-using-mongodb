a5bdf33a44d56a7bb330c98332617b23e2af43ea
zakkriel <tobler.brian@gmail.com>
2021-01-09 14:53:54 +0100

    module2
    none

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 4000;
-
+//moongose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://Admin:N0MrB0nd@cluster0.nbuzg.mongodb.net/productsdb', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+})
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
